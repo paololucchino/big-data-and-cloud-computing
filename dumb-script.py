db593d9369b4a97f1c7b15a3150e84c4dc917a64
Paolo Lucchino <paololucchino@gmail.com>
2019-06-18 11:43:05 +0200

    Example for cluster

---
+++ dumb-script.py
@@ -0,0 +1,25 @@
+from pyspark import SparkConf, SparkContext
+from math import sqrt
+
+conf = SparkConf().setMaster().setAppName("OddNumberSums")
+sc = SparkContext(conf=conf)
+
+n = 50
+
+allNumbers = sc.parallelize(range(n*2))
+allNumbers.collect()
+
+keyByOddEven = allNumbers.map(lambda x: (x%2, x))
+keyByOddEven.collect()
+
+sumNumbers = keyByOddEven.reduceByKey(lambda x, y: x+y)
+sumNumbers.collect()
+
+sums = sumNumbers.collect()
+
+print('The sum of the first {} odd numbers is {}, which is the square of {}'.format(n,
+                                                                                   sums[1][1],
+                                                                                   sqrt(sums[1][1])))
+
+sc.cancelAllJobs()
+sc.stop()
